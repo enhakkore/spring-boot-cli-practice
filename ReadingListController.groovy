8773dd6b25e9a4af3dc77029ee755c3ca2eaecf7
enhakkore <ahn.ephraim@gmail.com>
2019-11-22 16:19:58 +0900

    Add several groovy and Fix typo of directory name

---
+++ ReadingListController.groovy
@@ -0,0 +1,25 @@
+@Controller
+@RequestMapping("/")
+class ReadingListController {
+
+    String reader = "craig"
+
+    @Autowired
+    ReadingListRepository readingListRepository
+
+    @RequestMapping(method=RequestMethod.GET)
+    def readerBooks(Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader)
+        if(readingList) {
+            model.addAttribute("books", readingList)
+        }
+        "readingList"
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    def addToReadingList(Book book) {
+        book.setReader(reader)
+        readingListRepository.save(book)
+        "redirect:/"
+    }
+}
